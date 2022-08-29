7a6a709d05e914422bc952055a05412fc4523d11
Glauberdiasprado <glauberdiasprado@gmail.com>
2022-08-28 22:16:11 -0300

    firebase com javaScript

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,20 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.3/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAii_J0cf3vyE1-2CLyxddobhEskn1DdZQ",
+    authDomain: "mundo-invertido-ccbc4.firebaseapp.com",
+    projectId: "mundo-invertido-ccbc4",
+    storageBucket: "mundo-invertido-ccbc4.appspot.com",
+    messagingSenderId: "588251070022",
+    appId: "1:588251070022:web:662ab1afbc101ef20fd447"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  export default app
